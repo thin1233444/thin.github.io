7157e119fd909ac211411fb74e581406b45a3a7a
thin1233444 <114590343+thin1233444@users.noreply.github.com>
2022-09-28 22:51:47 +0700

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,8 @@
+function openFunction(){
+    document.getElementById('side-menu').style.width="200px";
+    document.getElementById('menu-bar').style.marginLeft="200px";
+}
+function closeFunction(){
+    document.getElementById('side-menu').style.width="0px"
+    document.getElementById('menu-bar').style.marginLeft="50px";
+}
